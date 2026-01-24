7cf2c6e33ef508f9eb06287ddd7b0c71cd760597
MaxenceLgt <maxence.largeot@epitech.eu>
2026-01-24 09:11:37 +0100

    docs(version.go): minimal version DTO doc

--- types/version.go
+++ types/version.go
@@ -1,9 +1,11 @@
 package types
 
+// VersionResponse represents the response from the Proxmox /version endpoint.
 type VersionResponse struct {
 	Data VersionData `json:"data"`
 }
 
+// VersionData contains Proxmox version information.
 type VersionData struct {
 	Version string `json:"version"`
 	Release string `json:"release"`
