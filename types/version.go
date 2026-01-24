1047bd57d3b5537332050d70e26c5756d6fe47dc
MaxenceLgt <maxence.largeot@epitech.eu>
2026-01-24 09:15:14 +0100

    feat(version.go): console optional parameter for version endpoint

--- types/version.go
+++ types/version.go
@@ -1,5 +1,14 @@
 package types
 
+type VersionConsole string
+
+const (
+	Applet  VersionConsole = "applet"
+	VV      VersionConsole = "vv"
+	HTML5   VersionConsole = "html5"
+	XTERMJS VersionConsole = "xtermjs"
+)
+
 // VersionResponse represents the response from the Proxmox /version endpoint.
 type VersionResponse struct {
 	Data VersionData `json:"data"`
@@ -7,7 +16,8 @@ type VersionResponse struct {
 
 // VersionData contains Proxmox version information.
 type VersionData struct {
-	Version string `json:"version"`
-	Release string `json:"release"`
-	RepoID  string `json:"repoid"`
+	Version string         `json:"version"`
+	Release string         `json:"release"`
+	RepoID  string         `json:"repoid"`
+	Console VersionConsole `json:"console"`
 }
